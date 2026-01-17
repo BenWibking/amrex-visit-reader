85c00108fc03f5a501d3016bea796e105de6e7ce
Ben Wibking <ben@wibking.com>
2026-01-16 19:20:23 -0500

    fix build

--- build_andes.sh
+++ build_andes.sh
@@ -11,6 +11,7 @@ cmake -S . -B build \
   -DVISIT_ROOT_INCLUDE_DIR=/sw/andes/visit/3.4.2/linux-x86_64/include \
   -DVISIT_LIBRARY_DIR=/sw/andes/visit/3.4.2/linux-x86_64/lib \
   -DVISIT_BINARY_DIR=/sw/andes/visit/3.4.2/linux-x86_64/bin \
+  -DMPI_LIBRARY_DIR=/sw/sources/visit/andes/3.4.2/build_dir/third_party/mpich/3.3.1/linux-x86_64_gcc-8.5/lib \
   -DAMReX_PIC=ON -DAMReX_BUILD_SHARED_LIBS=ON $1
 cmake --build build -j16
 
