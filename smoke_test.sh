4e8f1b1ef484190679d0b1e33d72b69a8f035abc
Ben Wibking <ben@wibking.com>
2026-01-30 13:12:08 -0500

    fix smoke tests

--- smoke_test.sh
+++ smoke_test.sh
@@ -2,5 +2,6 @@
 
 set -x
 /Applications/VisIt.app/Contents/Resources/bin/visit -cli -nowin -s example_data/check_parallel_plotfile_open.py <<'EOF'
-Exit()
+import sys
+sys.exit(0)
 EOF
--- smoke_test_particles.sh
+++ smoke_test_particles.sh
@@ -3,10 +3,12 @@
 set -x
 plotfile="${1:-example_data/Nyx_LyA/plt00000}"
 /Applications/VisIt.app/Contents/Resources/bin/visit -cli -nowin -s example_data/check_particle_plotfile_open.py "$plotfile" <<'EOF'
-Exit()
+import sys
+sys.exit(0)
 EOF
 
 plotfile="example_data/DiskGalaxy/plt0000020"
 /Applications/VisIt.app/Contents/Resources/bin/visit -cli -nowin -s example_data/check_particle_plotfile_open.py "$plotfile" <<'EOF'
-Exit()
+import sys
+sys.exit(0)
 EOF
